ea89a873171b2d6d559bc88fdb381674b2f1a0f4
edvard.ananyan <edvard.ananyan@92d1f0aa-4b89-11de-8ddf-d3afb9ac165e>
2009-05-21 03:50:46 +0000

    Initial commit

---
+++ playground/people/edo888/admin/toolbar.wcp.html.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @version   $Id$
+ * @copyright Copyright (C) 2009 Edvard Ananyan. All rights reserved.
+ * @license   http://www.gnu.org/licenses/gpl-2.0.html GNU/GPL
+ */
+
+// no direct access
+defined('_JEXEC') or die('Restricted access');
+
+class TOOLBAR_config {
+	function _DEFAULT() {
+		JToolBarHelper::help('screen.wcp');
+	}
+}
