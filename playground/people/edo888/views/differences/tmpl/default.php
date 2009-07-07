1c6ea0b990396af58bd0ebb34516ed86daf063bd
edo888 <edo888@92d1f0aa-4b89-11de-8ddf-d3afb9ac165e>
2009-07-07 06:19:27 +0000

    getDifferences bug fix, added new function to show table differences

--- playground/people/edo888/views/differences/tmpl/default.php
+++ playground/people/edo888/views/differences/tmpl/default.php
@@ -19,10 +19,10 @@ $document->addStyleDeclaration('.icon-32-refresh {background-image:url(./templat
                 <?php echo JText::_('NUM'); ?>
             </th>
             <th width="20">
-                <input type="checkbox" name="toggle" value="" onclick="checkAll(<?php echo count($this->items); ?>);" />
+                <input type="checkbox" name="toggle" value="" onclick="checkAll(<?php echo count($this->items)+count($this->table_items); ?>);" />
             </th>
             <th>
-                <?php echo JText::_('Path'); ?>
+                <?php echo JText::_('Path') . ' / ' . JText::_('Table Event'); ?>
             </th>
             <th width="180">
                 <?php echo JText::_('Modified Date'); ?>
@@ -30,12 +30,12 @@ $document->addStyleDeclaration('.icon-32-refresh {background-image:url(./templat
         </tr>
     </thead>
     <?php
-    $k = 0;
+    $k = $i = 0;
     for($i = 0, $n = count($this->items); $i < $n; $i++) {
         $row = new JObject;
         $row->set('path', $this->items[$i][0]);
         $row->set('mdate', $this->items[$i][1]);
-        $checked = JHTML::_('grid.checkedout', $row, $i, 'path');
+        $checked = JHTML::_('grid.id', $i, $row->path);
         ?>
         <tr class="<?php echo "row$k"; ?>">
             <td>
@@ -46,6 +46,31 @@ $document->addStyleDeclaration('.icon-32-refresh {background-image:url(./templat
             </td>
             <td>
                 <?php echo $row->path; ?>
+            </td>
+            <td>
+                <?php echo $row->mdate; ?>
+            </td>
+        </tr>
+        <?php
+        $k = 1 - $k;
+    }
+
+    for($j = 0, $n = count($this->table_items); $j < $n; $j++, $i++) {
+        $row = new JObject;
+        $row->set('id', $this->table_items[$j]->id);
+        $row->set('action', '<font color="#cc0000">'.$this->table_items[$j]->action . '</font> <b>' . $this->table_items[$j]->table_name . '</b>.<i>' . $this->table_items[$j]->table_key . '</i> = ' . $this->table_items[$j]->value);
+        $row->set('mdate', date('r', $this->table_items[$j]->mdate));
+        $checked = JHTML::_('grid.id', $i, $row->id);
+        ?>
+        <tr class="<?php echo "row$k"; ?>">
+            <td>
+                <?php echo $i + 1; ?>
+            </td>
+            <td>
+                <?php echo $checked; ?>
+            </td>
+            <td>
+                <?php echo $row->action; ?>
             </td>
             <td>
                 <?php echo $row->mdate; ?>
