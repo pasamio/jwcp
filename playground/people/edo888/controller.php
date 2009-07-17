f35efd0d38adeb8372d62b095782b540910ed573
edo888 <edo888@92d1f0aa-4b89-11de-8ddf-d3afb9ac165e>
2009-07-17 03:27:13 +0000

    Moved removeChild from controller to helper

--- playground/people/edo888/controller.php
+++ playground/people/edo888/controller.php
@@ -71,39 +71,7 @@ class WCPController extends JController {
     }
 
     function remove() {
-        // TODO: write the remove function
-        $db =& JFactory::getDBO();
-        $wcp_table = new TableWCP($db);
-
-        $cid = JRequest::getVar('cid');
-        foreach($cid as $id) {
-            $wcp_table->load($id);
-            // Debug: echo '<pre>', print_r($wcp_table, true), '</pre>';
-
-            // Delete tables
-            $params = new JParameter($wcp_table->params);
-            $database = json_decode($params->get('database'));
-            // Debug: echo '<pre>', print_r($database, true), '</pre>';
-            $child_db = new JDatabaseMySQL(array('host' => $database->host, 'user' => $database->user, 'password' => $database->password, 'database' => $database->database, 'prefix' => $database->prefix));
-            // Debug: $child_db->debug(1);
-
-            $child_tables = $child_db->getTableList();
-            // Debug: echo '<pre>', print_r($child_tables, true), '</pre>';
-            foreach($child_tables as $child_table) {
-                if(substr($child_table, 0, 4) == $database->prefix) {
-                    $child_db->setQuery('drop table ' . $child_table);
-                    $child_db->query();
-                }
-            }
-
-            // Delete files
-            if($wcp_table->path != '')
-                JFolder::delete(JPATH_ROOT.DS.$wcp_table->path);
-
-            // Delete database entry
-            $wcp_table->delete($id);
-        }
-
+        WCPHelper::removeChild();
         $this->setRedirect('index.php?option=com_wcp', JText::_('Child(s) deleted successfully'));
     }
 
