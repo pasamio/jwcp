8c2e5c5f1010ead3b7b2f7365b337ca217206e96
edo888 <edo888@92d1f0aa-4b89-11de-8ddf-d3afb9ac165e>
2009-07-31 02:57:34 +0000

    Added Disclaimer of Warranty from GNU/GPL license

--- playground/people/edo888/views/applypatch/tmpl/default.php
+++ playground/people/edo888/views/applypatch/tmpl/default.php
@@ -2,7 +2,13 @@
 /**
  * @version   $Id$
  * @copyright Copyright (C) 2009 Edvard Ananyan. All rights reserved.
+ * @author    Edvard Ananyan <edo888@gmail.com>
  * @license   http://www.gnu.org/licenses/gpl-2.0.html GNU/GPL
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
  */
 
 // no direct access
