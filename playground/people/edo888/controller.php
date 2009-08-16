ab4e41a3c7d46d6a4068cdc3ba4182dcf6c04989
edo888 <edo888@92d1f0aa-4b89-11de-8ddf-d3afb9ac165e>
2009-08-16 04:30:04 +0000

    Documentation started

--- playground/people/edo888/controller.php
+++ playground/people/edo888/controller.php
@@ -22,6 +22,11 @@ jimport('joomla.application.component.controller');
  */
 class WCPController extends JController {
 
+    /**
+     * Constructor
+     *
+     * @params array Controller configuration array
+     */
     function __construct($config = array()) {
         parent::__construct($config);
 
@@ -47,16 +52,25 @@ class WCPController extends JController {
     }
 
 
+    /**
+     * Displays the childs view
+     */
     function display() {
         JRequest::setVar('view', 'childs');
 	    parent::display();
 	}
 
+	/**
+	 * Displays the child view
+	 */
     function edit() {
         JRequest::setVar('view', 'child');
         parent::display();
     }
 
+    /**
+     * Creates/Saves the child
+     */
     function save() {
         list($cid) = JRequest::getVar('cid', array(''));
         if($cid == '') {
@@ -71,22 +85,34 @@ class WCPController extends JController {
         }
     }
 
+    /**
+     * Cancels the edit operation
+     */
     function cancel() {
         // Check for request forgeries
         JRequest::checkToken() or jexit('Invalid Token');
         $this->setRedirect('index.php?option=com_wcp');
     }
 
+    /**
+     * Removes the child(s)
+     */
     function remove() {
         WCPHelper::removeChild();
         $this->setRedirect('index.php?option=com_wcp', JText::_('Child(s) deleted successfully'));
     }
 
+    /**
+     * Displays the differences view
+     */
     function differences() {
         JRequest::setVar('view', 'differences');
         parent::display();
     }
 
+    /**
+     * Refreshes the cache
+     */
     function refreshDiff() {
         $cache =& JFactory::getCache('com_wcp', 'callback', 'file');
         $cache->clean('com_wcp', 'group');
@@ -96,12 +122,18 @@ class WCPController extends JController {
         $this->setRedirect('index.php?option=com_wcp&task=differences', JText::_('List Refreshed'));
     }
 
+    /**
+     * Creates a patch
+     */
     function createPatch() {
         WCPHelper::createPatch();
 
         //$this->setRedirect('index.php?option=com_wcp&task=differences', JText::_('Patch Created'));
     }
 
+    /**
+     * Applies a patch to the site
+     */
     function applyPatch() {
         if(!JRequest::getVar('submitted', false)) {
             JRequest::setVar('view', 'applyPatch');
@@ -117,6 +149,9 @@ class WCPController extends JController {
         }
     }
 
+    /**
+     * Commits changes to the master
+     */
     function commit() {
         WCPHelper::commit();
 
@@ -126,11 +161,17 @@ class WCPController extends JController {
         $this->setRedirect('index.php?option=com_wcp&task=differences', JText::_('Commit completed'));
     }
 
+    /**
+     * Merges 2 children
+     */
     function merge() {
         WCPHelper::merge();
         $this->setRedirect('index.php?option=com_wcp', JText::_('Childs merged successfully'));
     }
 
+    /**
+     * Reverts made changes
+     */
     function revertChild() {
         WCPHelper::revertChild();
 
@@ -140,6 +181,9 @@ class WCPController extends JController {
         $this->setRedirect('index.php?option=com_wcp&task=differences', JText::_('Revert completed'));
     }
 
+    /**
+     * Synchronizes the child with the master
+     */
     function syncChild() {
         WCPHelper::syncChild();
 
